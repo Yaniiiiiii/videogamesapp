9fbdf994040a114dfb2b437e31051c6104a9f62f
“YaniraZapata” <yanirazapata.c@gmail.com>
2023-02-06 12:19:59 +0100

    📝  add browser router and provider

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Menu from "./components/menu/menu";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Menu></Menu>
+      </header>
+    </div>
+  );
+}
+
+export default App;
